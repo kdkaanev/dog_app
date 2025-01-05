aec7fda451d524759d4b710e233ed4f8042c6536
kdkaanev <kdkaane@github.io>
2025-01-05 18:27:48 +0200

    up

--- front-end/dog-app/src/pages/home/HomePage.vue
+++ front-end/dog-app/src/pages/home/HomePage.vue
@@ -1,6 +1,19 @@
+<script setup>
+
+import NavHeader from './NavHeader.vue';
+
+
+</script>
+
+
+
+
+
+
 <template>
     <div>
         <h1>Home Page</h1>
         <p>Welcome to the Dog App!</p>
     </div>
+    <NavHeader />
 </template>
---
+++ front-end/dog-app/src/pages/home/NavHeader.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div>
+        <h1>NavHeader</h1>
+        <p>NavHeader to the Dog App!</p>
+    </div>
+    <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/users">Users</router-link></li>
+        <li><router-link to="/lost">Dogs</router-link></li>
+    </ul>
+</template>
