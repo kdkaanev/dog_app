cbc9d061e303f8ac2faa6abad33467460f8408ac
kdkaanev <kdkaane@github.io>
2025-01-08 19:12:31 +0200

    up

---
+++ front-end/dog-app/src/pages/dogs/components/PostCart.vue
@@ -0,0 +1,32 @@
+<template>
+
+
+
+
+  <div class="cart">
+        <div class="catr-pics">
+            <img class="pics"  src="https://img.freepik.com/free-vector/flat-chihuahua-silhouette_23-2150346441.jpg?t=st=1736242964~exp=1736246564~hmac=601eb78949914a0a27390f7e2b9457edea4ac71a5b38bbae4a49cd7317ea457c&w=740" alt="chihua">
+            <h2>Name</h2>
+        </div>
+       
+      </div>
+
+</template>
+
+
+
+
+<style scoped>
+
+.pics{
+   
+width: 100%;
+padding: 1rem;
+}
+.cart{
+    border-radius: 8px;
+    width: 240px;
+    background-color:rgb(238 222 196);
+    text-align: center;
+}
+</style>
